cb3835005a9ca15e0eb2f08c6fa4fb181e5f62b1
Abderrazek <abderrazek.younsi@gmail.com>
2023-05-26 07:24:15 +0100

    highlight the 'Articles' link in the navigation bar when the 'Overloop Tech Test' page is selected

--- frontend/src/components/MainNav/MainNav.jsx
+++ frontend/src/components/MainNav/MainNav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
@@ -10,6 +10,8 @@ import {
 } from "../../constants";
 
 function MainNav() {
+  const location = useLocation();
+
   return (
     <div className="MainNav">
       <Navbar bg="dark" variant="dark">
@@ -17,10 +19,18 @@ function MainNav() {
           Overloop Tech Test
         </Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to={ROUTE_ARTICLE_LIST}>
+          <Nav.Link
+            as={Link}
+            to={ROUTE_ARTICLE_LIST}
+            className={location.pathname === ROUTE_ARTICLE_LIST ? "active" : ""}
+          >
             Articles
           </Nav.Link>
-          <Nav.Link as={Link} to={ROUTE_AUTHOR_LIST}>
+          <Nav.Link
+            as={Link}
+            to={ROUTE_AUTHOR_LIST}
+            className={location.pathname === ROUTE_AUTHOR_LIST ? "active" : ""}
+          >
             Authors
           </Nav.Link>
         </Nav>
